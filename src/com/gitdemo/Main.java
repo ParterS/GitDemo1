6c67c6aa91bc1ee11b95eb7bfcb0642726166262
ParterS <retr16@meta.ua>
2022-12-21 17:22:31 -0400

    Add honda, add honda cost, add showing totalCost, hondaCost.

--- src/com/gitdemo/Main.java
+++ src/com/gitdemo/Main.java
@@ -5,12 +5,15 @@ public class Main {
         Car car1 = new Car("Toyota",2023,23500);
         Car car2 = new Car("Mazda",2022,24600);
         Car car3 = new Car("Honda",2023,28200);
-        Car car4 = new Car("Honda",2023,28200);
+        Car car4 = new Car("Nissan",2023,28200);
 
-    double totalCost = car1.getCost()+car2.getCost()+car3.getCost();
+    double totalCost = car1.getCost()+car2.getCost()+car3.getCost()+car4.getCost();
         double mazdaCost = car2.getCost();
-
+        double hondaCost = car3.getCost();
         System.out.println(mazdaCost);
+        System.out.println(hondaCost);
+        System.out.println(totalCost);
         System.out.println("Hellorld!");
+
     }
 }
